0267ca040d81d7e02509bda026936d6734cb3ba6
Hazmirul Afiq <IceM4nn@users.noreply.github.com>
2022-03-23 11:14:42 +0800

    Added ping from IP list in a file.

--- ping-sweep.bat
+++ ping-sweep.bat
@@ -9,6 +9,12 @@ if ["%help%"]==["true"] (
 	goto :usage
 )
 
+IF ["%1"]==["-f"] (
+  SET file=%~f2
+  SHIFT
+  goto :ip_list
+)
+
 set ip=%1
 for /F "tokens=1,2,3,4 delims=." %%a in ("%ip%") do (
 	set ipaddr=%%a.%%b.%%c
@@ -22,15 +28,27 @@ for /F "tokens=1,2 delims=-" %%a in ("%range%") do (
 :: echo [*] start address:  %startaddr%
 :: echo [*] end address:  %endaddr%
 
-for /L %%i IN (%startaddr%,1,%endaddr%) do echo [+] pinging address %ipaddr%.%%i && ping -n 1 %ipaddr%.%%i | FIND /i "Reply from %ipaddr%.%%i" >nul && echo     - Host is up
+for /L %%i IN (%startaddr%,1,%endaddr%) do (
+	echo [+] pinging address %ipaddr%.%%i && ping -n 1 %ipaddr%.%%i | FIND /i "Reply from %ipaddr%.%%i" >nul && echo     - Host is up
+)
 exit /B 0
+
+:: Ping sweep from IP list file.
+
+:ip_list
+for /F "tokens=*" %%A in (%file%) do (
+	echo [+] pinging address %%A && ping -n 1 %%A | FIND /i "Reply from %%A" >nul && echo     - Host is up 
+)
+exit /B 0
+goto :eof
 
 :: Help section
 
 :usage
-echo Usage: ping-sweep.bat ^<ip-address-range^>
+echo Usage: ping-sweep.bat ^<ip-address-range^>^|^<-f file_name.txt^>
 echo Perform ping sweep on windows internal network.
 echo.
 echo   ip-address-range	            IP address in range. For example: 192.168.1.1-10
+echo   -f file_name.txt	            IP address list in a file.
 echo   -h, --help                   display this help and exit
 goto :eof
